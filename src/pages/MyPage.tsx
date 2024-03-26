f0bdc0e7da9a4b89d9aaa95a84b365caf35e7ffa
murat.acar <murat.acar@live.se>
2024-03-26 22:03:19 +0100

    murat tredje commit
    Co-authored-by: SepVaz <SepVaz@users.noreply.github.com>
    Co-authored-by: Michel Gorjizadeh <michel.gorjizadeh@utb.ecutbildning.se>
    Co-authored-by: Johan Nihlén <johan.nihlen@utb.ecutbildning.se>
    Co-authored-by: Jakob  Malm <jakob.malm@utb.ecutbildning.se>

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -4,7 +4,11 @@ import axios from "axios";
 import { ICleaners } from "../Cleaners";
 
 function MyPage() {
-  const [data, setData] = useState<ICleaners[]>([]);
+  const [cleaners, setCleaners] = useState<string[]>([]);
+  const [selectedCleaners, setSelectedCleaners] = useState<string[]>([]);
+  const [selectedCleaning, setSelectedCleaning] = useState<string>("Basic");
+  const [showBooking, setShowBooking] = useState<ICleaners[]>([]);
+  const [data, setData] = useState<ICleaners[]>([]); //använd till senare för att visa bokningarna
   const location = useLocation();
   const navigate = useNavigate();
   const { name } = location.state || { name: "Användare" };
@@ -12,10 +16,10 @@ function MyPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-      
         const response = await axios.get("/data.json");
-        const cleanersData = response.data.data.cleaner
-        setData(cleanersData); 
+        const cleanersData = response.data.data.cleaner;
+        const cleanersName = cleanersData.map((cleaner: any) => cleaner.name);
+        setCleaners(cleanersName);
       } catch (err) {
         console.error("fetch failed", err);
       }
@@ -28,11 +32,45 @@ function MyPage() {
     navigate("/");
   }
 
+  //informerar typescript vilken typ av elemt händelsen hanteras för, typescript kan då ge bättre stöd när man skriver koden. gör också att vi undviker misstag om vi endast använder de egenskaper som metoden faktiskt är till för
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const addBooking = {
+      ...showBooking,
+      id: Date.now(), date: new Date().toLocaleDateString('sv-SE', { year: 'numeric', month: '2-digit', day: '2-digit' })
+    };
+
+    const createBooking = async () => {
+      try {
+        const response = await axios.post('data.json', addBooking)
+        setShowBooking((prev) => [...prev, response.data.data.booking] )
+      } catch (err) {
+        console.log("Failed to create booking", err);
+      }
+    }
+    createBooking();
+  };
+
+  
+
   return (
     <div>
       <h3>Välkommen till {name}s sida</h3>
       <button onClick={handleLogout}>Gå tillbaka</button>
       <div>
+        <form onSubmit={handleSubmit}>
+          <select name="" id="">
+            <option value="" disabled selected hidden>
+              Välj städare
+            </option>
+            {cleaners.map((cleaner) => (
+              <option key={cleaner} value={cleaner}>
+                {cleaner}
+              </option>
+            ))}
+          </select>
+          <button type="submit">Boka</button>
+        </form>
         <h1>Bookings</h1>
         {data.map((cleaner) => (
           <div key={cleaner.id}>
